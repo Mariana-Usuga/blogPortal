011ea72541b0f03065b1c4730743b8e2e7560d95
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-21 09:55:47 -0500

    fix

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ function connectDB(app) {
   //app.use(mongoSanitize());
   app.use(express.json());
   app.use(morgan('dev'));
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
 }
 
